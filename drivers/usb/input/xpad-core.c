2cef04d5ddd1f8b26fa5fd13c611413757d6da57
sny23 <sny23>
2004-03-03 21:27:20 +0000

    Xpad-0.1.4 port from 2.4 to 2.6.
    Currently only the joystick and mouse devices supported.
    DVD dongle will be a separate module again later.
    
    I noticed a possible problem with the mouse support. If you happen to
    encounter that the buttons work, while moving the pointer doesn't, try to
    reload the module and tell me about the problem.

---
+++ drivers/usb/input/xpad-core.c
@@ -0,0 +1,547 @@
+/*
+ * Xbox input device driver for Linux - v0.1.4
+ *
+ * Copyright (c)  2002 - 2004  Marko Friedemann <mfr@bmx-chemnitz.de>
+ *
+ *	Contributors:
+ *		Vojtech Pavlik <vojtech@suse.sz>,
+ *		Oliver Schwartz <Oliver.Schwartz@gmx.de>,
+ *		Steven Toth <steve@toth.demon.co.uk>,
+ *		Franz Lehner <franz@caos.at>,
+ *		Ivan Hawkes <blackhawk@ivanhawkes.com>
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *
+ * This driver is based on:
+ *  - information from     http://euc.jp/periphs/xbox-controller.en.html
+ *  - the iForce driver    drivers/char/joystick/iforce.c
+ *  - the skeleton-driver  drivers/usb/usb-skeleton.c
+ *
+ * Thanks to:
+ *  - ITO Takayuki for providing essential xpad information on his website
+ *  - Vojtech Pavlik     - iforce driver / input subsystem
+ *  - Greg Kroah-Hartman - usb-skeleton driver
+ *
+ * TODO:
+ *  - fine tune axes
+ *  - fine tune mouse behaviour (should not do linear acceleration)
+ *  - NEW: get rumble working correctly, fix all the bugs and support multiple
+ *         simultaneous effects
+ *  - NEW: split funtionality mouse/joustick into two source files
+ *  - NEW: implement /proc interface (toggle mouse/rumble enable/disable, etc.)
+ *  - NEW: implement user space daemon application that handles that interface
+ *
+ * History: moved to end of file
+ */
+ 
+#include <linux/config.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/slab.h>
+#include <linux/module.h>
+#include <linux/smp_lock.h>
+#include <linux/usb.h>
+#include <linux/version.h>
+#include <linux/timer.h>
+#include <asm/uaccess.h>
+
+#include "xpad.h"
+
+
+static struct xpad_device xpad_device[] = {
+	/* please keep those ordered wrt. vendor/product ids
+	  vendor, product, name                  	     */
+	{ 0x045e, 0x0202, "Microsoft Xbox Controller" },
+	{ 0x045e, 0x0285, "Microsoft Xbox Controller S" },
+	{ 0x045e, 0x0289, "Microsoft Xbox Controller S" }, /* microsoft is stupid */
+	{ 0x05fd, 0x1007, "Mad Catz Controller" }, /* CHECKME: this seems strange */
+	{ 0x05fd, 0x107a, "InterAct PowerPad Pro" },
+	{ 0x0738, 0x4536, "Jess Technologies (Mad Catz) MicroCON" },
+	{ 0x0c12, 0x9902, "HAMA VibraX - lucky boy" }, /* these are broken */
+	{ 0x0e6f, 0x0003, "Logic3 Freebird wireless Controller" },
+	{ 0x0f30, 0x0202, "Joytech Advanced Controller" },
+	{ 0xffff, 0xffff, "Chinese crap Xbox Controller" }, /* WTF are device IDs for? */
+	{ 0x0000, 0x0000, "nothing detected - FAIL" }
+};
+
+static signed short xpad_btn[] = {
+	BTN_A, BTN_B, BTN_C, BTN_X, BTN_Y, BTN_Z,	/* analog buttons */
+	BTN_START, BTN_BACK, BTN_THUMBL, BTN_THUMBR,	/* start/back/sticks */
+	-1						/* terminating entry */
+};
+
+static signed short xpad_abs[] = {
+	ABS_X, ABS_Y,		/* left stick */
+	ABS_RX, ABS_RY,		/* right stick */
+	ABS_Z, ABS_RZ,		/* triggers left/right */
+	ABS_HAT0X, ABS_HAT0Y,	/* digital pad */
+	ABS_HAT1X, ABS_HAT1Y,	/* analog buttons A + B */
+	ABS_HAT2X, ABS_HAT2Y,	/* analog buttons C + X */
+	ABS_HAT3X, ABS_HAT3Y,	/* analog buttons Y + Z */
+	-1			/* terminating entry */
+};
+
+static struct usb_device_id xpad_table [] = {
+	{ USB_INTERFACE_INFO('X', 'B', 0) },	/* Xbox USB-IF not approved class */
+	{ USB_INTERFACE_INFO( 3 ,  0 , 0) },	/* for Joytech Advanced Controller */
+	{ }
+};
+
+MODULE_DEVICE_TABLE(usb, xpad_table);
+
+/**
+ *	xpad_process_packet
+ *
+ *	Completes a request by converting the data into events
+ *	for the input subsystem.
+ *
+ *	The report descriptor was taken from ITO Takayukis website:
+ *	 http://euc.jp/periphs/xbox-controller.en.html
+ */
+static void xpad_process_packet(struct usb_xpad *xpad, u16 cmd, unsigned char *data, struct pt_regs *regs)
+{
+	struct input_dev *dev = &xpad->dev;
+	
+	input_regs(dev, regs);
+	
+	/* left stick */
+	input_report_abs(dev, ABS_X, ((__s16) (((__s16)data[13] << 8) | data[12])));
+	input_report_abs(dev, ABS_Y, ((__s16) (((__s16)data[15] << 8) | data[14])));
+	
+	/* right stick */
+	input_report_abs(dev, ABS_RX, ((__s16) (((__s16)data[17] << 8) | data[16])));
+	input_report_abs(dev, ABS_RY, ((__s16) (((__s16)data[19] << 8) | data[18])));
+   	
+   	/* triggers left/right */
+	input_report_abs(dev, ABS_Z, data[10]);
+	input_report_abs(dev, ABS_RZ, data[11]);
+	
+	/* digital pad: bits (3 2 1 0) (right left down up) */
+	input_report_abs(dev, ABS_HAT0X, !!(data[2] & 0x08) - !!(data[2] & 0x04));
+	input_report_abs(dev, ABS_HAT0Y, !!(data[2] & 0x01) - !!(data[2] & 0x02));
+
+	/* start/back buttons and stick press left/right */
+	input_report_key(dev, BTN_START, (data[2] & 0x10) >> 4);
+	input_report_key(dev, BTN_BACK, (data[2] & 0x20) >> 5);
+	input_report_key(dev, BTN_THUMBL, (data[2] & 0x40) >> 6);
+	input_report_key(dev, BTN_THUMBR, data[2] >> 7);
+	
+	/* button A digital/analog mode */
+	input_report_key(dev, BTN_A, data[4]);
+	input_report_abs(dev, ABS_HAT1X, data[4]);
+	
+	/* button B digital/analog mode */
+	input_report_key(dev, BTN_B, data[5]);
+	input_report_abs(dev, ABS_HAT1Y, data[5]);
+	
+	/* button C (black) digital/analog mode */
+	input_report_key(dev, BTN_C, data[8]);
+	input_report_abs(dev, ABS_HAT2X, data[8]);
+	
+	/* button X digital/analog mode */
+	input_report_key(dev, BTN_X, data[6]);
+	input_report_abs(dev, ABS_HAT2Y, data[6]);
+	
+	/* button Y digital/analog mode */
+	input_report_key(dev, BTN_Y, data[7]);
+	input_report_abs(dev, ABS_HAT3X, data[7]);
+
+	/* button Z (white) digital/analog mode */
+	input_report_key(dev, BTN_Z, data[9]);
+	input_report_abs(dev, ABS_HAT3Y, data[9]);
+	
+	input_sync(dev);
+	
+	/* process input data for mouse event generation */
+	xpad_mouse_process_packet(xpad, cmd, data);
+}
+
+/**
+ *	xpad_irq_in
+ *
+ *	Completion handler for interrupt in transfers (user input).
+ *	Just calls xpad_process_packet which does then emit input events.
+ */
+static void xpad_irq_in(struct urb *urb, struct pt_regs *regs)
+{
+	struct usb_xpad *xpad = urb->context;
+	int retval;
+	
+	switch (urb->status) {
+	case 0:
+		/* success */
+		break;
+	case -ECONNRESET:
+	case -ENOENT:
+	case -ESHUTDOWN:
+		/* this urb is terminated, clean up */
+		dbg("%s - urb shutting down with status: %d",
+		    __FUNCTION__, urb->status);
+		return;
+	default:
+		dbg("%s - nonzero urb status received: %d",
+		    __FUNCTION__, urb->status);
+		goto exit;
+	}
+	
+	xpad_process_packet(xpad, 0, xpad->idata, regs);
+	
+exit:
+	retval = usb_submit_urb(urb, GFP_ATOMIC);
+	if (retval)
+		err("%s - usb_submit_urb failed with result %d",
+		    __FUNCTION__, retval);
+}
+
+/*	xpad_init_urb
+ *
+ *	initialize the input urb
+ *	this is to be called when joystick or mouse device are opened
+ */
+int xpad_start_urb(struct usb_xpad *xpad)
+{
+	int status;
+	
+	// check if joystick or mouse device are opened
+	if (xpad->open_count + xpad->mouse_open_count > 0)
+		return 0;
+
+	xpad->irq_in->dev = xpad->udev;
+	if ((status = usb_submit_urb(xpad->irq_in, GFP_KERNEL))) {
+		err("open input urb failed: %d", status);
+		return -EIO;
+	}
+	
+	return 0;
+}
+
+/**
+ *	xpad_open
+ *
+ *	Called when a an application opens the device.
+ */
+static int xpad_open(struct input_dev *dev)
+{
+	struct usb_xpad *xpad = dev->private;
+	int status;
+	
+	if (xpad->open_count)
+		return 0;
+		
+	info("opening device");
+	
+	if ((status = xpad_start_urb(xpad)))
+		return status;
+		
+	++xpad->open_count;
+
+	xpad_rumble_open(xpad);
+	
+	return 0;
+}
+
+void xpad_stop_urb(struct usb_xpad *xpad)
+{
+	if (xpad->open_count + xpad->mouse_open_count > 0)
+		return;
+	
+	usb_unlink_urb(xpad->irq_in);
+}
+
+/**
+ *	xpad_close
+ *
+ *	Called when an application closes the device.
+ */
+static void xpad_close(struct input_dev *dev)
+{
+	struct usb_xpad *xpad = dev->private;
+	
+	if (--xpad->open_count)
+		return;
+	
+	info("closing device");
+	
+	xpad_stop_urb(xpad);
+	xpad_rumble_close(xpad);
+}
+
+/**	xpad_init_input_device
+ *
+ *	setup the input device for the kernel
+ */
+static void xpad_init_input_device(struct usb_interface *intf, struct xpad_device device)
+{
+	struct usb_xpad *xpad = usb_get_intfdata(intf);
+	struct usb_device *udev = interface_to_usbdev(intf);
+	char path[64];
+	int i;
+	
+	xpad->dev.id.bustype = BUS_USB;
+	xpad->dev.id.vendor = udev->descriptor.idVendor;
+	xpad->dev.id.product = udev->descriptor.idProduct;
+	xpad->dev.id.version = udev->descriptor.bcdDevice;
+	xpad->dev.dev = &intf->dev;
+	xpad->dev.private = xpad;
+	xpad->dev.name = device.name;
+	xpad->dev.phys = xpad->phys;
+	xpad->dev.open = xpad_open;
+	xpad->dev.close = xpad_close;
+	
+	usb_make_path(udev, path, 64);
+	snprintf(xpad->phys, 64, "%s/input0", path);
+	
+	/* this was meant to allow a user space tool on-the-fly configuration
+	   of driver options (mouse on, rumble on, etc)
+	   yet, Vojtech said this is better done using sysfs (linux 2.6)
+	   plus, it needs a patch to the input subsystem */
+//	xpad->dev.ioctl = xpad_ioctl;
+
+	xpad->dev.evbit[0] = BIT(EV_KEY) | BIT(EV_ABS);
+	
+	for (i = 0; xpad_btn[i] >= 0; ++i)
+		set_bit(xpad_btn[i], xpad->dev.keybit);
+	
+	for (i = 0; xpad_abs[i] >= 0; ++i) {
+		
+		signed short t = xpad_abs[i];
+		
+		set_bit(t, xpad->dev.absbit);
+		
+		switch (t) {
+			case ABS_X:
+			case ABS_Y:
+			case ABS_RX:
+			case ABS_RY:	/* the two sticks */
+				xpad->dev.absmax[t] =  32767;
+				xpad->dev.absmin[t] = -32768;
+				xpad->dev.absflat[t] = 128;
+				xpad->dev.absfuzz[t] = 16;
+				break;
+			case ABS_Z:	/* left trigger */
+			case ABS_RZ:	/* right trigger */
+			case ABS_HAT1X:	/* analog button A */
+			case ABS_HAT1Y:	/* analog button B */
+			case ABS_HAT2X:	/* analog button C */
+			case ABS_HAT2Y:	/* analog button X */
+			case ABS_HAT3X:	/* analog button Y */
+			case ABS_HAT3Y:	/* analog button Z */
+				xpad->dev.absmax[t] = 255;
+				xpad->dev.absmin[t] = 0;
+				break;
+			case ABS_HAT0X:
+			case ABS_HAT0Y:	/* the d-pad */
+				xpad->dev.absmax[t] =  1;
+				xpad->dev.absmin[t] = -1;
+				break;
+		}
+	}
+	
+	if (xpad_rumble_probe(udev, xpad, ifnum) != 0)
+		err("could not init rumble");
+
+	input_register_device(&xpad->dev);
+	printk(KERN_INFO "input: %s on %s\n", xpad->dev.name, path);
+}
+
+/**
+ *	xpad_probe
+ *
+ *	Called upon device detection to find a suitable driver.
+ *	Must return NULL when no xpad is found, else setup everything.
+ */
+static int xpad_probe(struct usb_interface *intf, const struct usb_device_id *id)
+{
+	struct usb_device *udev = interface_to_usbdev(intf);
+	struct usb_xpad *xpad = NULL;
+	struct usb_endpoint_descriptor *ep_irq_in;
+	int i;
+	int probedDevNum = -1;	/* this takes the index into the known devices
+				   array for the recognized device */
+	
+	// try to detect the device we are called for
+	for (i = 0; xpad_device[i].idVendor; ++i) {
+		if ((udev->descriptor.idVendor == xpad_device[i].idVendor) &&
+		    (udev->descriptor.idProduct == xpad_device[i].idProduct)) {
+			probedDevNum = i;
+			break;
+		}
+	}
+	
+	// sanity check, did we recognize this device? if not, fail
+	if ((probedDevNum == -1) || (xpad_device[probedDevNum].idVendor ==
+	    xpad_device[probedDevNum].idVendor == 0))
+		return -ENODEV;
+		
+	if ((xpad = kmalloc (sizeof(struct usb_xpad), GFP_KERNEL)) == NULL) {
+		err("cannot allocate memory for new pad");
+		return -ENOMEM;
+	}
+	memset(xpad, 0, sizeof(struct usb_xpad));
+	
+	xpad->idata = usb_buffer_alloc(udev, XPAD_PKT_LEN,
+				       SLAB_ATOMIC, &xpad->idata_dma);
+	if (!xpad->idata) {
+		kfree(xpad);
+		return -ENOMEM;
+	}
+	
+	/* setup input interrupt pipe (button and axis state) */
+	xpad->irq_in = usb_alloc_urb(0, GFP_KERNEL);
+        if (!xpad->irq_in) {
+		err("cannot allocate memory for new pad irq urb");
+		usb_buffer_free(udev, XPAD_PKT_LEN, xpad->idata, xpad->idata_dma);
+                kfree(xpad);
+                return -ENOMEM;
+	}
+	
+	ep_irq_in = &intf->altsetting[0].endpoint[0].desc;
+	
+	xpad->udev = udev;
+	
+	
+	/* init input URB for USB INT transfer from device */
+	usb_fill_int_urb(xpad->irq_in, udev,
+			 usb_rcvintpipe(udev, ep_irq_in->bEndpointAddress),
+			 xpad->idata, XPAD_PKT_LEN,
+			 xpad_irq_in, xpad, ep_irq_in->bInterval);
+	xpad->irq_in->transfer_dma = xpad->idata_dma;
+	xpad->irq_in->transfer_flags |= URB_NO_TRANSFER_DMA_MAP;
+	
+	// we set this here so we can extract it in the two functions below
+	usb_set_intfdata(intf, xpad);
+	
+	xpad_init_input_device(intf, xpad_device[probedDevNum]);
+	xpad_mouse_init_input_device(intf, xpad_device[probedDevNum]);
+	
+	return 0;
+}
+
+/**
+ *	xpad_disconnect
+ *
+ *	Called upon device disconnect to dispose of the structures and
+ *	close the USB connections.
+ */
+static void xpad_disconnect(struct usb_interface *intf)
+{
+	struct usb_xpad *xpad = usb_get_intfdata(intf);
+	
+	usb_set_intfdata(intf, NULL);
+	if (xpad) {
+		info( "disconnecting device" );
+		usb_unlink_urb(xpad->irq_in);
+		xpad_rumble_close(xpad);
+		input_unregister_device(&xpad->dev);
+		
+		usb_free_urb(xpad->irq_in);
+		usb_buffer_free(interface_to_usbdev(intf), XPAD_PKT_LEN,
+				xpad->idata, xpad->idata_dma);
+	
+		xpad_rumble_disconnect(xpad);
+		xpad_mouse_cleanup(xpad);
+		
+		kfree(xpad);
+	}
+}
+
+/******************* Linux driver framework specific stuff ************/
+
+static struct usb_driver xpad_driver = {
+	.owner		= THIS_MODULE,
+	.name		= "xpad",
+	.probe		= xpad_probe,
+	.disconnect	= xpad_disconnect,
+	.id_table	= xpad_table,
+};
+
+/**
+ * driver init entry point
+ */
+static int __init usb_xpad_init(void)
+{
+	int result = usb_register(&xpad_driver);
+	if (result == 0)
+		info(DRIVER_DESC " " DRIVER_VERSION);
+	return result;
+}
+
+/**
+ * driver exit entry point
+ */
+static void __exit usb_xpad_exit(void)
+{
+	usb_deregister(&xpad_driver);
+}
+
+module_init(usb_xpad_init);
+module_exit(usb_xpad_exit);
+
+MODULE_AUTHOR(DRIVER_AUTHOR);
+MODULE_DESCRIPTION(DRIVER_DESC);
+MODULE_LICENSE("GPL");
+
+/*
+ *  driver history
+ * ----------------
+ *
+ * 2003-05-15 - 0.1.2 : ioctls, dynamic mouse/rumble activation, /proc fs
+ *  - added some /proc files for informational purposes (readonly right now)
+ *  - added init parameters for mouse/rumble activation upon detection
+ *  - added dynamic changes to mouse events / rumble effect generation via
+ *    ioctls - NOTE: this requires a currently unofficial joydev patch!
+ *
+ * 2003-04-29 - 0.1.1 : minor cleanups, some comments
+ *  - fixed incorrect handling of unknown devices (please try ir dongle now)
+ *  - fixed input URB length (the 256 bytes from 0.1.0 broke everything for the
+ *    MS controller as well as my Interact device, set back to 32 (please
+ *    REPORT problems BEFORE any further changes here, since those can be fatal)
+ *  - fixed rumbling for MS controllers (need 6 bytes output report)
+ *  - dropped kernel-2.5 ifdefs, much more readable now
+ *  - preparation for major rework under way, stay tuned
+ *
+ * 2003-03-25 - 0.1.0 : (Franz) Some Debuggin
+ *  - Better Handling
+ *  - X/Y support, Speed differenting
+ *  - Landing Zone, Dead Zone, Offset kompensation, Zero-adjustment, .... aso.
+ *  - Removed Wheel handling in Mouse Emulation .. sensless..
+ *
+ * 2003-01-23 - 0.1.0-pre : added mouse emulation and rumble support
+ *  - can provide mouse emulation (compile time switch)
+ *    this code has been taken from Oliver Schwartz' xpad-mouse driver
+ *  - basic rumble support (compile time switch)        EXPERIMENTAL!  
+ *
+ * 2002-08-05 - 0.0.6 : added analog button support
+ *
+ * 2002-07-17 - 0.0.5 : (Vojtech Pavlik) rework
+ *  - simplified d-pad handling
+ *
+ * 2002-07-16 - 0.0.4 : minor changes, merge with Vojtech's v0.0.3
+ *  - verified the lack of HID and report descriptors
+ *  - verified that ALL buttons WORK
+ *  - fixed d-pad to axes mapping
+ *
+ * 2002-07-14 - 0.0.3 : (Vojtech Pavlik) rework
+ *  - indentation fixes
+ *  - usb + input init sequence fixes
+ *
+ * 2002-07-02 - 0.0.2 : basic working version
+ *  - all axes and 9 of the 10 buttons work (german InterAct device)
+ *  - the black button does not work
+ *
+ * 2002-06-27 - 0.0.1 : first version, just said "XBOX HID controller"
+ */
---
+++ drivers/usb/input/xpad.h
@@ -0,0 +1,168 @@
+/*
+ * Xbox Controller driver for Linux - v0.1.4
+ *
+ *	header file containing ioctl definitions
+ *
+ * Copyright (c)  2003  Marko Friedemann <mfr@bmx-chemnitz.de>
+ *
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+ 
+#ifndef __XPAD_h
+#define __XPAD_h
+
+
+/*********** ioctl stuff, can be used outside of the driver ***********/
+#define USB_XPAD_IOC_MAGIC 	'x'
+
+#define USB_XPAD_IOCRESET 	_IO(  USB_XPAD_IOC_MAGIC, 0 )
+#define USB_XPAD_IOCSMOUSE 	_IOW( USB_XPAD_IOC_MAGIC, 1, int )
+#define USB_XPAD_IOCGMOUSE 	_IOR( USB_XPAD_IOC_MAGIC, 2, int )
+#define USB_XPAD_IOCSRUMBLE 	_IOW( USB_XPAD_IOC_MAGIC, 3, int )
+#define USB_XPAD_IOCGRUMBLE 	_IOR( USB_XPAD_IOC_MAGIC, 4, int )
+
+#define USB_XPAD_IOCSIR 	_IOW( USB_XPAD_IOC_MAGIC, 5, int )
+#define USB_XPAD_IOCGIR 	_IOR( USB_XPAD_IOC_MAGIC, 6, int )
+
+#define USB_XPAD_IOC_MAXNR 	6
+
+
+/************************* driver internals ***************************/
+#ifdef __KERNEL__
+
+#include <linux/input.h>
+#include <linux/circ_buf.h>
+
+/****************** driver description and version ********************/
+#define DRIVER_VERSION		"v0.1.4"
+#define DRIVER_AUTHOR		"Marko Friedemann <mfr@bmx-chemnitz.de>,\
+ Oliver Schwartz <Oliver.Schwartz@gmx.de>, Georg Lukas <georg@op-co.de>"
+
+#ifdef CONFIG_USB_XPAD_MOUSE
+#define DRIVER_DESC		"driver for Xbox controllers with mouse emulation"
+#else
+#define DRIVER_DESC		"driver for Xbox controllers"
+#endif
+
+/****************************** constants *****************************/
+#define XPAD_MAX_DEVICES	4
+#define XPAD_PKT_LEN		32	/* input packet size */
+#define XPAD_PKT_LEN_IR		6	/* input packet size - IR dongle */
+#define XPAD_PKT_LEN_FF		6	/* output packet size - rumble */
+
+#define XPAD_TX_BUFSIZE		XPAD_PKT_LEN_FF * 8	/* max. 8 requests */
+
+/************************* the device struct **************************/
+struct usb_xpad {
+	struct input_dev dev;			/* input device interface */
+	struct usb_device *udev;		/* usb device */
+	
+	struct urb *irq_in;			/* urb for int. in report */
+	unsigned char *idata;			/* input data */
+	dma_addr_t idata_dma;
+	
+	char phys[65];				/* physical input dev path */
+	
+	int open_count;				/* reference count */
+
+	unsigned char offsetset_compensation;
+	int left_offset_x;
+	int left_offset_y;
+	int right_offset_x;
+	int right_offset_y;
+	
+	int isDVDKit;				/* is this a DVD dongle IR? */
+	struct circ_buf rx;			/* ring buffer for IR input */
+
+#ifdef CONFIG_USB_XPAD_RUMBLE
+	int rumble_enabled;			/* ioctl can toggle rumble */
+	
+	int ep_out_adr;				/* number of out endpoint */
+	unsigned char tx_data[XPAD_PKT_LEN_FF];	/* output data (rumble) */
+	int strong_rumble, play_strong;		/* strong rumbling */
+	int weak_rumble, play_weak;		/* weak rumbling */
+	struct timer_list rumble_timer;		/* timed urb out retry */
+	wait_queue_head_t wait;			/* wait for URBs on queue */
+	
+	spinlock_t tx_lock;
+	struct circ_buf tx;
+	unsigned char tx_buf[XPAD_TX_BUFSIZE];
+	long tx_flags[1];			/* transmit flags */
+#endif
+	
+#ifdef CONFIG_USB_XPAD_MOUSE
+	struct input_dev dev_mouse;		/* mouse device interface */
+	int mouse_open_count;			/* reference count */
+	int mouse_enabled;			/* ioctl can toggle rumble */
+	
+	int rel_x;
+	int rel_y;
+	int rel_wheel;
+	int rel_wheeltimer;
+	struct timer_list timer;		/* timed mouse input events */
+#endif
+};
+
+/* for the list of know devices */
+struct xpad_device {
+	u16 idVendor;
+	u16 idProduct;
+	char *name;
+};
+
+#ifdef __USB_XPAD_MOUSE
+ extern int xpad_start_urb(struct usb_xpad *xpad);
+ extern void xpad_stop_urb(struct usb_xpad *xpad);
+#endif
+
+/************************ mouse function stubs ************************/
+#ifndef CONFIG_USB_XPAD_MOUSE
+ #define mouse_open_count open_count
+ #define xpad_mouse_process_packet(xpad, cmd, data) {}
+ #define xpad_mouse_init_input_device(intf, device) {}
+ #define xpad_mouse_cleanup(xpad) {}
+#else /* CONFIG_USB_XPAD_MOUSE */
+ #ifndef __USB_XPAD_MOUSE
+  extern void xpad_mouse_process_packet(struct usb_xpad *xpad, u16 cmd, unsigned char *data);
+  extern void xpad_mouse_init_input_device(struct usb_interface *intf, struct xpad_device device);
+  extern void xpad_mouse_cleanup(struct usb_xpad *xpad);
+ #endif /* __USB_XPAD_MOUSE */
+#endif /* CONFIG_USB_XPAD_MOUSE */
+
+/************************ rumble function stubs ***********************/
+#ifndef CONFIG_USB_XPAD_RUMBLE
+ #define xpad_rumble_ioctl(dev, cmd, arg) -ENOTTY
+ #define xpad_rumble_open(xpad) {}
+ #define xpad_rumble_probe(udev, xpad, ifnum) 0
+ #define xpad_rumble_close(xpad) {}
+ #define xpad_rumble_disconnect(xpad) {}
+#else /* CONFIG_USB_XPAD_RUMBLE */
+
+ #define XPAD_TX_RUNNING	0
+ #define XPAD_TX_INC(var, n)	(var) += n; (var) %= XPAD_TX_BUFSIZE
+
+ #ifndef __USB_XPAD_RUMBLE
+  extern int  xpad_rumble_ioctl(struct input_dev *dev, unsigned int cmd, unsigned long arg);
+  extern void xpad_rumble_open(struct usb_xpad *xpad);
+  extern int  xpad_rumble_probe(struct usb_device *udev, struct usb_xpad *xpad, unsigned int ifnum);
+  extern void xpad_rumble_close(struct usb_xpad *xpad);
+  extern void xpad_rumble_disconnect(struct usb_xpad *xpad);
+ #endif /* __USB_XPAD_RUMBLE */
+#endif /* CONFIG_USB_XPAD_RUMBLE */
+
+#endif /* __KERNEL__ */
+
+#endif /* __XPAD_h */
