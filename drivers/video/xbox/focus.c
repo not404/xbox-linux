ba9e33e448df03218095fb8ca5c52b1264974a2d
davidmpye <davidmpye>
2004-04-12 14:41:30 +0000

    Corrections

--- drivers/video/xbox/focus.c
+++ drivers/video/xbox/focus.c
@@ -192,7 +192,7 @@ int focus_calc_mode(xbox_video_mode * mode, struct riva_regs * riva_out)
 		case TV_ENC_NTSC:
 			tv_vtotal=525;
 			tv_vactive=480;			
-			tv_hactive = 702;
+			tv_hactive = 710;
 			tv_htotal  = 858;
 			regs[0x0d] &= ~0x01;
 			regs[0x40] = 0x21;
@@ -227,7 +227,7 @@ int focus_calc_mode(xbox_video_mode * mode, struct riva_regs * riva_out)
 			/* Default to PAL */
 			tv_vtotal = 625;
 			tv_vactive = 576;
-			tv_hactive = 720;
+			tv_hactive = 702;
 			tv_htotal = 864;
 			break;
 	}
