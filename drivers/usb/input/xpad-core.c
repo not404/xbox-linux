ab2d2d9dd7eca71f4676e952e4b1795f3e3c56e1
davidmpye <davidmpye>
2004-05-03 20:41:09 +0000

    Sync with 2.4 again

--- drivers/usb/input/xpad-core.c
+++ drivers/usb/input/xpad-core.c
@@ -79,6 +79,7 @@ static struct xpad_device xpad_device[] = {
 	{ 0x0738, 0x4556, 0, "Mad Catz Lynx Wireless Controller" },
 	{ 0x0738, 0x6040, 1, "Mad Catz Beat Pad Pro" },
 	{ 0x0c12, 0x9902, 0, "HAMA VibraX - *FAULTY HARDWARE*" }, /* these are broken */
+	{ 0x0e4c, 0x2390, 0, "Radica Games Jtech Controller"},	
 	{ 0x0e6f, 0x0003, 0, "Logic3 Freebird wireless Controller" },
 	{ 0x0f30, 0x0202, 0, "Joytech Advanced Controller" },
 	{ 0x12ab, 0x8809, 1, "Xbox DDR dancepad" },
