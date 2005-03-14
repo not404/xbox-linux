d8fe1e08a4b25ec70aaa9a021a59719519193a24
shallax <shallax>
2005-03-14 17:44:38 +0000

    Make some stuff debug-only.  When rebooting from within Xorg - after sysklogd has been stopped, the console would get an ugly printk displayed.  Now this shouldn't happen.

--- drivers/video/xbox/fbdev.c
+++ drivers/video/xbox/fbdev.c
@@ -1020,63 +1020,79 @@ static int xboxfb_do_maximize(struct fb_info *info,
 	NVTRACE_ENTER();
 	/* use highest possible virtual resolution */
 	if (var->xres_virtual == -1 && var->yres_virtual == -1) {
+#ifdef CONFIG_FB_RIVA_DEBUG
 		printk(KERN_WARNING PFX
 		       "using maximum available virtual resolution\n");
+#endif
 		for (i = 0; modes[i].xres != -1; i++) {
 			if (modes[i].xres * nom / den * modes[i].yres <
 			    info->fix.smem_len)
 				break;
 		}
 		if (modes[i].xres == -1) {
-			printk(KERN_ERR PFX
+#ifdef CONFIG_FB_RIVA_DEBUG
+		printk(KERN_ERR PFX
 			       "could not find a virtual resolution that fits into video memory!!\n");
+#endif
 			NVTRACE("EXIT - EINVAL error\n");
 			return -EINVAL;
 		}
 		var->xres_virtual = modes[i].xres;
 		var->yres_virtual = modes[i].yres;
 
-		printk(KERN_INFO PFX
-		       "virtual resolution set to maximum of %dx%d\n",
-		       var->xres_virtual, var->yres_virtual);
+		//printk(KERN_INFO PFX
+		//       "virtual resolution set to maximum of %dx%d\n",
+		//       var->xres_virtual, var->yres_virtual);
 	} else if (var->xres_virtual == -1) {
 		var->xres_virtual = (info->fix.smem_len * den /
 			(nom * var->yres_virtual)) & ~15;
+#ifdef CONFIG_FB_RIVA_DEBUG
 		printk(KERN_WARNING PFX
 		       "setting virtual X resolution to %d\n", var->xres_virtual);
+#endif
 	} else if (var->yres_virtual == -1) {
 		var->xres_virtual = (var->xres_virtual + 15) & ~15;
 		var->yres_virtual = info->fix.smem_len * den /
 			(nom * var->xres_virtual);
+#ifdef CONFIG_FB_RIVA_DEBUG
 		printk(KERN_WARNING PFX
 		       "setting virtual Y resolution to %d\n", var->yres_virtual);
+#endif
 	} else {
 		var->xres_virtual = (var->xres_virtual + 15) & ~15;
 		if (var->xres_virtual * nom / den * var->yres_virtual > info->fix.smem_len) {
+#ifdef CONFIG_FB_RIVA_DEBUG
 			printk(KERN_ERR PFX
 			       "mode %dx%dx%d rejected...resolution too high to fit into video memory!\n",
 			       var->xres, var->yres, var->bits_per_pixel);
+#endif
 			NVTRACE("EXIT - EINVAL error\n");
 			return -EINVAL;
 		}
 	}
 	
 	if (var->xres_virtual * nom / den >= 8192) {
+#ifdef CONFIG_FB_RIVA_DEBUG
 		printk(KERN_WARNING PFX
 		       "virtual X resolution (%d) is too high, lowering to %d\n",
 		       var->xres_virtual, 8192 * den / nom - 16);
+#endif
 		var->xres_virtual = 8192 * den / nom - 16;
 	}
 	
 	if (var->xres_virtual < var->xres) {
+#ifdef CONFIG_FB_RIVA_DEBUG
 		printk(KERN_ERR PFX
 		       "virtual X resolution (%d) is smaller than real\n", var->xres_virtual);
+#endif
 		return -EINVAL;
 	}
 
 	if (var->yres_virtual < var->yres) {
+#ifdef CONFIG_FB_RIVA_DEBUG
 		printk(KERN_ERR PFX
 		       "virtual Y resolution (%d) is smaller than real\n", var->yres_virtual);
+#endif
 		return -EINVAL;
 	}
 	if (var->yres_virtual > 0x7fff/nom)
