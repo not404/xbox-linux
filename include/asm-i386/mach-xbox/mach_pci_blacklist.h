279328b6c77b075e183f27bfa23814572f747d6b
aothieno <aothieno>
2005-09-05 13:27:22 +0000

    Do not make the test for blacklisted PCI devices conditional on `machine_is_xbox'. This is already expanded correctly when mcore-y := mach-xbox (arch/i386/Makefile). Otherwise, gcc says:
      arch/i386/pci/direct.c: In function 'pci_conf1_read':
      arch/i386/pci/direct.c:25: warning: control may reach end of non-void function 'mach_pci_is_blacklisted' being inlined

--- include/asm-i386/mach-xbox/mach_pci_blacklist.h
+++ include/asm-i386/mach-xbox/mach_pci_blacklist.h
@@ -1,14 +1,10 @@
 #ifndef __ASM_MACH_PCI_BLACKLIST_H
 #define __ASM_MACH_PCI_BLACKLIST_H
 
-#include <linux/xbox.h>
-
 static inline int mach_pci_is_blacklisted(int bus, int dev, int fn)
 {
-	if(machine_is_xbox) {
-		return (bus > 1) || ((bus != 0) && ((dev != 0) || (fn != 0))) ||
-			(!bus && !dev && ((fn == 1) || (fn == 2)));
-	}
+	return (bus > 1) || ((bus != 0) && ((dev != 0) || (fn != 0))) ||
+		(!bus && !dev && ((fn == 1) || (fn == 2)));
 }
 
 #endif
