a86ba79fd88c019c3bee645e8c213cf85dcc6a87
Laurence Lee <lalee@ufo2.lalee.net>
2009-05-24 22:00:14 -1000

    LALEE: MACH-XBOX - Conform to 2.6.18 changes to Mach initialization.

--- arch/i386/mach-xbox/setup.c
+++ arch/i386/mach-xbox/setup.c
@@ -2,13 +2,13 @@
  *	Machine specific setup for xbox
  */
 
-#include <linux/config.h>
 #include <linux/init.h>
 #include <linux/irq.h>
 #include <linux/interrupt.h>
 #include <asm/acpi.h>
 #include <asm/arch_hooks.h>
-
+#include <asm/e820.h>
+#include <asm/setup.h>
 /**
  * pre_intr_init_hook - initialisation prior to setting up interrupt vectors
  *
@@ -91,4 +91,43 @@ static struct irqaction irq0  = {
 void __init time_init_hook(void)
 {
 	setup_irq(0, &irq0);
+}
+
+
+/**
+ * machine_specific_memory_setup - Hook for machine specific memory setup.
+ *
+ * Description:
+ *      This is included late in kernel/setup.c so that it can make
+ *      use of all of the static functions.
+ **/
+
+char * __init machine_specific_memory_setup(void)
+{
+        char *who;
+        who = "BIOS-e820";
+        /*
+         * Try to copy the BIOS-supplied E820-map.
+         *
+         * Otherwise fake a memory map; one section from 0k->640k,
+         * the next section from 1mb->appropriate_mem_k
+         */
+        sanitize_e820_map(E820_MAP, &E820_MAP_NR);
+        if (copy_e820_map(E820_MAP, E820_MAP_NR) < 0) {
+                unsigned long mem_size;
+
+                /* compare results from other methods and take the greater */
+                if (ALT_MEM_K < EXT_MEM_K) {
+                        mem_size = EXT_MEM_K;
+                        who = "BIOS-88";
+                } else {
+                        mem_size = ALT_MEM_K;
+                        who = "BIOS-e801";
+                }
+
+                e820.nr_map = 0;
+                add_memory_region(0, LOWMEMSIZE(), E820_RAM);
+                add_memory_region(HIGH_MEMORY, mem_size << 10, E820_RAM);
+        }
+        return who;
 }
